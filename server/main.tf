e54c2f2f4e41557a2a7998881cf690bc06026954
chavo1 <42143239+chavo1@users.noreply.github.com>
2018-08-28 08:28:03 +0300

    Create main.tf

---
+++ server/main.tf
@@ -0,0 +1,27 @@
+resource "aws_key_pair" "chavo" {
+  key_name   = "chavo"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-04169656fea786776"
+  instance_type = "t2.micro"
+  key_name      = "${aws_key_pair.chavo.id}"
+
+  connection {
+    user        = "ubuntu"
+    private_key = "${file("~/.ssh/id_rsa")}"
+  }
+
+  provisioner "file" {
+    source      = "asset"
+    destination = "/tmp"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/asset/setup-web.sh",
+      "sudo /tmp/asset/setup-web.sh args",
+    ]
+  }
+}
