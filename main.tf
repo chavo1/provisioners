5868fa9daf5d240ef8b10dc15e4d74235f8e76f8
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2018-08-28 08:33:14 +0200

    remove extra aws_instance
    not needed for the goal of the repo

--- main.tf
+++ main.tf
@@ -14,17 +14,3 @@ provider "aws" {
 module "server" {
   source = "server"
 }
-
-resource "aws_instance" "example" {
-  ami           = "ami-6871a115"
-  instance_type = "t2.micro"
-  key_name      = "id_rsa"
-}
-
-output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
-}
-
-output "public_dns" {
-  value = "${aws_instance.example.public_dns}"
-}
